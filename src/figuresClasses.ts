55d3c45e7f1b21e01075efbcd015b68aad47c863
Евгений Котвицкий <ekotvizky@gmail.com>
2025-04-15 15:31:07 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,101 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
 
-export class Circle implements Figure {}
+  getArea(): number;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  readonly shape: Shape = 'triangle';
 
-export function getInfo(figure): string {
-  return typeof figure;
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.checkSides();
+  }
+
+  protected checkSides(): void {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw Error('All sides should be more than 0');
+    }
+
+    if (this.semiPerimeter <= Math.max(this.a, this.b, this.c)) {
+      throw Error(
+        `sides ${this.a}, ${this.b} and ${this.c} can't form a triangle`,
+      );
+    }
+  }
+
+  public get semiPerimeter(): number {
+    return (this.a + this.b + this.c) / 2;
+  }
+
+  public getArea(): number {
+    return (
+      Math.floor(
+        Math.sqrt(
+          this.semiPerimeter *
+            (this.semiPerimeter - this.a) *
+            (this.semiPerimeter - this.b) *
+            (this.semiPerimeter - this.c),
+        ) * 100,
+      ) / 100
+    );
+  }
+}
+
+export class Circle implements Figure {
+  readonly shape: Shape = 'circle';
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    this.checkRadius();
+  }
+
+  protected checkRadius(): void {
+    if (this.radius <= 0) {
+      throw Error('Radius should be more than 0');
+    }
+  }
+
+  public getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  readonly shape: Shape = 'rectangle';
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    this.checkSides();
+  }
+
+  protected checkSides(): void {
+    if (this.width <= 0 || this.height <= 0) {
+      throw Error('All sides should be more than 0');
+    }
+  }
+
+  public getArea(): number {
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
